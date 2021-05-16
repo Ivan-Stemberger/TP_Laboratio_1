5200410cb42d17905de91c0b7e68d9ea3a50fda1
Ivan-Stemberger <71237941+Ivan-Stemberger@users.noreply.github.com>
2021-05-15 23:34:18 -0300

    Delete src directory

--- src/mostrar.h
+++
@@ -1,8 +0,0 @@
-#ifndef MOSTRAR_H_
-#define MOSTRAR_H_
-
-void mostrarMenu(float operando1, float operando2);
-void mostrarResultados(float suma, float resta, float multiplicacion, float division, int factorialA, int factorialB, float operando2);
-
-
-#endif /* MOSTRAR_H_ */
--- src/validaciones.h
+++
@@ -1,6 +0,0 @@
-#ifndef VALIDACIONES_H_
-#define VALIDACIONES_H_
-
-int esNumerico(char auxiliar[]);
-
-#endif /* VALIDACIONES_H_ */
